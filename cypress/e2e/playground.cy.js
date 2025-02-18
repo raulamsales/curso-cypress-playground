b7128a996f775fd6d17f34a3889e4c63ddf44e64
Raul Sales <raulamsales@gmail.com>
2025-02-18 17:04:18 -0300

    12. Clicando em elementos - exercício

--- cypress/e2e/playground.cy.js
+++ cypress/e2e/playground.cy.js
@@ -1,5 +1,22 @@
+beforeEach(() => {
+  cy.visit('https://cypress-playground.s3.eu-central-1.amazonaws.com/index.html')
+});
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+
+  it('show promo banner', () => {
+    cy.get('#promotional-banner')
+    .should('be.visible')
+  });
+  it.only('teste que clica no botão Subscribe', () => {
+    cy.clock()
+    cy.get('button[type="submit"]')
+    .should('contain', 'Subscribe')
+    .click()
+
+    cy.get('#success')
+    .should('be.visible')
+    cy.tick(3000)
+    cy.get('#success')
+    .should('not.be.visible')
+  });
 })
