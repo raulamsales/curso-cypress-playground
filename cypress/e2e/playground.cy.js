fc474eaf18dbab07a1fcb6012c2ddfcbba0d3aa5
Raul Sales <raulamsales@gmail.com>
2025-05-15 15:33:56 -0300

    Curso Finalizado

--- cypress/e2e/playground.cy.js
+++ cypress/e2e/playground.cy.js
@@ -1,5 +1,7 @@
 beforeEach(() => {
-  cy.visit('https://cypress-playground.s3.eu-central-1.amazonaws.com/index.html')
+      //const now = new Date(Date.UTC(2024, 5, 16, 14, 30, 0, 0))
+      //cy.clock(now)
+      cy.visit('https://cypress-playground.s3.eu-central-1.amazonaws.com/index.html')
 });
 describe('template spec', () => {
 
@@ -110,9 +112,56 @@ describe('template spec', () => {
     //cy.get('#intercept > .error').should('contain', 'Oops, something went wrong. Check your internet connection, refresh the page, and try again.')
   });
 
-  it.only('Criando um simples teste de API com Cypress', () => {
+  it('Criando um simples teste de API com Cypress', () => {
     cy.request('GET', 'https://jsonplaceholder.typicode.com/todos/1')
     .its('status')
     .should('be.equal', 200)
+  });
+
+  it('Experimente criando um teste que seleciona um nível invocando seu valor e acionando a mudança. Certifique-se de que o nível correto seja exibido.', () => {
+    cy.get('input[type="range"]')
+    .invoke('val', 5)
+    .trigger('change')
+  });
+  
+  Cypress._.times(10, index =>{
+    it(`testcase ${index + 1}`, () => {
+      cy.get('#level')
+      .invoke('val', index + 1)
+      .trigger('change')
+    });
+  })
+
+  it('Lidando com inputs do tipo date', () => {
+    cy.get('#date').type('2024-06-16').blur().should('have.value', '2024-06-16')
+  });
+
+  it('Protegendo dados sensíveis com Cypress', () => {
+    cy.get('#password').type(Cypress.env('password'), { delay: 0, log: false})
+    cy.get('#show-password-checkbox').check().should('be.checked')
+    cy.get('#password[type="text"]').should('exist')
+    cy.get('#password[type="password"]').should('not.exist')
+  });
+
+  it('Contando itens com Cypress', () => {
+    cy.get("ul#animals li").should('have.length', 5)
+  });
+
+  it('Congelando 🧊 o relógio ⌚ do navegador com Cypress', () => {
+    cy.get('#date-section-paragraph').should('contain', '2024-06-16')
+  });
+
+  it('Usando dados gerados pela aplicação como entrada para os testes', () => {
+    cy.get('#timestamp').then(element =>{
+      const value = element[0].innerText
+      cy.get('input#code').type(value)
+      cy.get('input#code').should('have.value', value)
+      
+    })
+  });
+
+  it.only("test that clicks the 'Download a text file' link. Make sure to read the file's content to check if it's correct.", () => {
+    cy.get('[download="example.txt"]').click()
+    cy.readFile('cypress/downloads/example.txt').should('contain', 'Hello, World!')
   });
 })
